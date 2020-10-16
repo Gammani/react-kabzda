efe97eea706ad5cbdd1ae16090650d0b3bd0f2e1
Oreda-Z <shtucer31@gmail.com>
2020-10-16 12:00:40 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,62 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
 
-    </div>
-  );
+function AppTitle() {
+    return (
+        <>This is APP component </>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <AccordionTitle />
+            <AccordionBody />
+        </div>
+    );
+}
+
+function AccordionTitle() {
+    return (
+        <h3>Меню</h3>
+    );
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
 }
 
 export default App;
