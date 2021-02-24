679b8205af967efbf4e09341afa2da25c204302a
Oreda-Z <shtucer31@gmail.com>
2021-02-24 16:33:52 +0300

    useEffect - компонента часы

---
+++ src/components/clock/clock.stories.tsx
@@ -0,0 +1,11 @@
+import {Clock} from "./clock";
+import React from "react";
+
+export default {
+    title: 'Clock',
+    component: Clock,
+}
+
+export const BaseExample = () => {
+    return <Clock/>
+};
